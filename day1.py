94572d29f79d22a858023bf25ad02d9527edc50e
Jim Newbery <james@tinnedfruit.com>
2022-12-01 21:19:00 +0000

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -12,28 +12,34 @@ input = """1000
 9000
 
 10000
+
 """
 
 
+def totals(input):
+    groups = [group.split("\n") for group in input.strip().split("\n\n")]
+    return sorted(sum([int(item) for item in group]) for group in groups)[::-1]
+
+
 def day1a(input):
-    totals = []
-    total = 0
-    for line in input.splitlines():
-        if line:
-            total += int(line)
-        else:
-            totals.append(total)
-            total = 0
-    totals.append(total)
-    return max(totals)
+    return totals(input)[0]
+
+
+def day1b(input):
+    return sum(totals(input)[0:3])
 
 
 def test_day1a():
     assert day1a(input) == 24_000
 
 
+def test_day1b():
+    assert day1b(input) == 45_000
+
+
 if __name__ == "__main__":
     with open("./day1.txt", "r") as file:
         data = file.read()
         print("Day 1a", day1a(data))
+        print("Day 1b", day1b(data))
         file.close()
