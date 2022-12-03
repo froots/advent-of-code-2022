640d6c2244bf18e2f838be007c483ab1fece0c45
Jim Newbery <james@tinnedfruit.com>
2022-12-03 16:43:39 +0000

    Refactor score_hand

--- day02.py
+++ day02.py
@@ -10,6 +10,8 @@ LOSE = 0
 
 
 def score_hand(opponent, me):
+    opponent = VALUES[opponent]
+    me = VALUES[me]
     mod_diff = (opponent - me) % 3
     if mod_diff == 0:
         return me + 1 + DRAW
@@ -19,28 +21,35 @@ def score_hand(opponent, me):
 
 
 def day2a(input):
-    strategy = [
-        (VALUES[line[0]], VALUES[line[2]]) for line in input.strip().splitlines()
-    ]
-    return sum(score_hand(opponent, me) for opponent, me in strategy)
+    return sum(score_hand(line[0], line[2]) for line in input.strip().splitlines())
+
+
+# def day2b(input):
+#     strategy = [
+#         (VALUES[line[0]], adjust(line[2], line[0])])
+#     ]
 
 
 def test_day2a():
     assert day2a(input) == 15
 
 
+# def test_day2b():
+#     assert day2b(input) == 12
+
+
 def test_score_hand():
-    assert score_hand(0, 0) == 4  # 0-0 % 3 == 0
-    assert score_hand(1, 1) == 5
-    assert score_hand(2, 2) == 6
+    assert score_hand("A", "X") == 4  # 0-0 % 3 == 0
+    assert score_hand("B", "Y") == 5
+    assert score_hand("C", "Z") == 6
 
-    assert score_hand(0, 1) == 8  # 0-1 % 3 == 2
-    assert score_hand(1, 2) == 9  # 1-2 % 3 == 2
-    assert score_hand(2, 0) == 7  # 2-0 % 3 == 2
+    assert score_hand("A", "Y") == 8  # 0-1 % 3 == 2
+    assert score_hand("B", "Z") == 9  # 1-2 % 3 == 2
+    assert score_hand("C", "X") == 7  # 2-0 % 3 == 2
 
-    assert score_hand(0, 2) == 3  # 0-2 % 3 == 1
-    assert score_hand(1, 0) == 1  # 1-0 % 3 == 1
-    assert score_hand(2, 1) == 2  # 2-1 % 3 == 1
+    assert score_hand("A", "Z") == 3  # 0-2 % 3 == 1
+    assert score_hand("B", "X") == 1  # 1-0 % 3 == 1
+    assert score_hand("C", "Y") == 2  # 2-1 % 3 == 1
 
 
 if __name__ == "__main__":
