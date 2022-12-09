9ef6d9d3da8a566564efb770054bd98021b363fb
Jim Newbery <james@tinnedfruit.com>
2022-12-09 17:19:22 +0000

    Refactor (slower!)

--- day08.py
+++ day08.py
@@ -8,61 +8,60 @@ example = """30373
 
 def parse(data):
     grid = {}
+
     lines = data.strip().splitlines()
 
-    for row, trees in enumerate(lines):
-        for column, tree in enumerate(trees):
-            grid[(column, row)] = int(tree)
+    for y, trees in enumerate(lines):
+        for x, tree in enumerate(trees):
+            grid[(x, y)] = int(tree)
 
     return grid, len(lines[0]), len(lines)
 
 
+def get_sightlines(x, y, grid, breadth, depth):
+    north = [(x, sy, grid[(x, sy)]) for sy in reversed(range(y))]
+    east = [(sx, y, grid[(sx, y)]) for sx in range(x + 1, breadth)]
+    south = [(x, sy, grid[(x, sy)]) for sy in range(y + 1, depth)]
+    west = [(sx, y, grid[(sx, y)]) for sx in reversed(range(x))]
+
+    return north, east, south, west
+
+
+def is_visible(x, y, grid, breadth, depth):
+    on_edge = x == 0 or y == 0 or x == breadth - 1 or y == depth - 1
+
+    if on_edge:
+        return True
+
+    sightlines = get_sightlines(x, y, grid, breadth, depth)
+
+    return any(
+        max(h for _, _, h in sightline) < grid[(x, y)] for sightline in sightlines
+    )
+
+
 def day8a(data):
-    grid, max_x, max_y = parse(data)
+    grid, breadth, depth = parse(data)
 
-    visible = set()
-
-    # l to r
-    for y in range(max_y):
-        vis_h = -1
-        for x in range(max_x):
-            h = grid.get((x, y))
-            if h > vis_h:
-                visible.add((x, y, h))
-                vis_h = h
-
-    # r to l
-    for y in range(max_y):
-        vis_h = -1
-        for x in reversed(range(max_x)):
-            h = grid.get((x, y))
-            if h > vis_h:
-                visible.add((x, y, h))
-                vis_h = h
-
-    # t to b
-    for x in range(max_x):
-        vis_h = -1
-        for y in range(max_y):
-            h = grid.get((x, y))
-            if h > vis_h:
-                visible.add((x, y, h))
-                vis_h = h
-
-    # b to t
-    for x in range(max_x):
-        vis_h = -1
-        for y in reversed(range(max_y)):
-            h = grid.get((x, y))
-            if h > vis_h:
-                visible.add((x, y, h))
-                vis_h = h
+    visible = {
+        (x, y, grid[(x, y)])
+        for y in range(depth)
+        for x in range(breadth)
+        if is_visible(x, y, grid, breadth, depth)
+    }
 
     return len(visible)
 
 
 def test_day8a():
     assert day8a(example) == 21
+
+
+def test_is_visible():
+    grid, breadth, depth = parse(example)
+
+    assert is_visible(1, 1, grid, breadth, depth) is True
+    assert is_visible(3, 1, grid, breadth, depth) is False
 
 
 def main():
