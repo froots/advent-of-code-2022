d651be620e79a254b01e0cb968833bb44b74a161
Jim Newbery <james@tinnedfruit.com>
2022-12-10 10:40:28 +0000

    Day 9 part 2

--- day09.py
+++ day09.py
@@ -8,6 +8,16 @@ L 5
 R 2
 """
 
+example2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+
 DIRECTIONS = {"U": (0, 1), "R": (1, 0), "D": (0, -1), "L": (-1, 0)}
 
 
@@ -59,9 +69,9 @@ def move_command(direction, distance, head, tails):
     return (hx, hy), tails, locations
 
 
-def day9a(data):
+def calculate_no_of_last_tail_locations(data, tail_count=1):
     head = (0, 0)
-    tails = [(0, 0)]
+    tails = [(0, 0)] * tail_count
 
     last_tail_locations = set(tails[-1])
 
@@ -73,8 +83,21 @@ def day9a(data):
     return len(last_tail_locations)
 
 
+def day9a(data):
+    return calculate_no_of_last_tail_locations(data)
+
+
+def day9b(data):
+    return calculate_no_of_last_tail_locations(data, 9)
+
+
 def test_day9a():
     assert day9a(example) == 13
+
+
+def test_day9b():
+    assert day9b(example) == 1
+    assert day9b(example2) == 36
 
 
 def test_move_command_with_one_tail():
@@ -93,6 +116,7 @@ def main():
     with open("day09.txt", "r", encoding="utf8") as file:
         data = file.read()
     print("Day 9a", day9a(data))
+    print("Day 9b", day9b(data))
 
 
 if __name__ == "__main__":
