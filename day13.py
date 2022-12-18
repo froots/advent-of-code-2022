94daa187f066d5b022c84d7d5589438305cf4101
James Newbery <james@tinnedfruit.com>
2022-12-18 08:08:08 +0000

    Day 13 WIP

---
+++ day13.py
@@ -0,0 +1,66 @@
+example = """[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+"""
+
+
+def compare(*values):
+    a, b = values
+
+    if isinstance(a, int) and isinstance(b, int):
+        return None if b == a else b > a
+
+    if len(a) and len(b):
+        next_a, a = a[0], a[1:]
+        next_b, b = b[0], b[1:]
+        c = compare(next_a, next_b)
+        return compare(a, b) if c is None else c
+
+
+def day13a(data):
+    packets = [
+        tuple(eval(packet) for packet in pair.split("\n"))
+        for pair in data.strip().split("\n\n")
+    ]
+
+
+def test_day13a():
+    assert day13a(example) == 13
+
+
+def test_compare_bare_integers():
+    assert compare(1, 3) is True
+    assert compare(3, 1) is False
+    assert compare(1, 1) is None
+
+
+def test_compare_integer_lists_with_one_item():
+    assert compare([1], [3]) is True
+    assert compare([3], [1]) is False
+    assert compare([1], [1]) is None
+
+
+def test_compare_integer_lists_with_equal_length():
+    assert compare([1, 2], [1, 3]) is True
+    assert compare([3, 2, 1], [3, 1, 3]) is False
+    assert compare([1, 2, 3], [1, 2, 3]) is None
