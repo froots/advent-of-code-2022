db102a0f243e0f5b885b3752fb2662a65f765966
Jim Newbery <james@tinnedfruit.com>
2022-12-18 10:07:21 +0000

    Day 13 part 2

--- day13.py
+++ day13.py
@@ -1,3 +1,5 @@
+import functools
+
 example = """[1,1,3,1,1]
 [1,1,5,1,1]
 
@@ -27,7 +29,7 @@ example = """[1,1,3,1,1]
 def compare(a, b):
 
     if isinstance(a, int) and isinstance(b, int):
-        return None if b == a else b > a
+        return b - a
 
     if isinstance(a, int) and isinstance(b, list):
         return compare([a], b)
@@ -39,12 +41,9 @@ def compare(a, b):
         next_a, a = a[0], a[1:]
         next_b, b = b[0], b[1:]
         c = compare(next_a, next_b)
-        return compare(a, b) if c is None else c
+        return compare(a, b) if c == 0 else c
 
-    if not len(a) and not len(b):
-        return None
-
-    return bool(len(b))
+    return len(b) - len(a)
 
 
 def day13a(data):
@@ -55,64 +54,80 @@ def day13a(data):
 
     orders = {i + 1: compare(*pair) for i, pair in enumerate(packets)}
 
-    return sum(i for i, order in orders.items() if order is True)
+    return sum(i for i, order in orders.items() if order >= 1)
+
+
+def day13b(data):
+    packets = [eval(packet) for packet in data.strip().split("\n") if packet]
+    dividers = [[[2]], [[6]]]
+    packets.extend(dividers)
+    packets.sort(key=functools.cmp_to_key(compare), reverse=True)
+
+    i1, i2 = (i + 1 for i, packet in enumerate(packets) if packet in dividers)
+
+    return i1 * i2
 
 
 def test_day13a():
     assert day13a(example) == 13
 
 
+def test_day13b():
+    assert day13b(example) == 140
+
+
 def test_compare_bare_integers():
-    assert compare(1, 3) is True
-    assert compare(3, 1) is False
-    assert compare(1, 1) is None
+    assert compare(1, 3) >= 1
+    assert compare(3, 1) <= -1
+    assert compare(1, 1) == 0
 
 
 def test_compare_integer_lists_with_one_item():
-    assert compare([1], [3]) is True
-    assert compare([3], [1]) is False
-    assert compare([1], [1]) is None
+    assert compare([1], [3]) >= 1
+    assert compare([3], [1]) <= -1
+    assert compare([1], [1]) == 0
 
 
 def test_compare_integer_lists_with_equal_length():
-    assert compare([1, 2], [1, 3]) is True
-    assert compare([3, 2, 1], [3, 1, 3]) is False
-    assert compare([1, 2, 3], [1, 2, 3]) is None
+    assert compare([1, 2], [1, 3]) >= 1
+    assert compare([3, 2, 1], [3, 1, 3]) <= -1
+    assert compare([1, 2, 3], [1, 2, 3]) == 0
 
 
 def test_compare_integer_with_list():
-    assert compare([3], 3) is None
-    assert compare(3, [3]) is None
-    assert compare(3, [4]) is True
-    assert compare([3], 4) is True
-    assert compare(4, [3]) is False
-    assert compare([4], 3) is False
+    assert compare([3], 3) == 0
+    assert compare(3, [3]) == 0
+    assert compare(3, [4]) >= 1
+    assert compare([3], 4) >= 1
+    assert compare(4, [3]) <= -1
+    assert compare([4], 3) <= -1
 
 
 def test_compare_lists_with_unequal_length():
-    assert compare([1, 2, 3], [1, 2]) is False
-    assert compare([1, 2], [1, 2, 3]) is True
+    assert compare([1, 2, 3], [1, 2]) <= -1
+    assert compare([1, 2], [1, 2, 3]) >= 1
 
 
 def test_compare_examples():
-    assert compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) is True
-    assert compare([[1], [2, 3, 4]], [[1], 4]) is True
-    assert compare([9], [[8, 7, 6]]) is False
-    assert compare([[4, 4], 4, 4], [[4, 4], 4, 4, 4]) is True
-    assert compare([7, 7, 7, 7], [7, 7, 7]) is False
-    assert compare([], [3]) is True
-    assert compare([[[]]], [[]]) is False
+    assert compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]) >= 1
+    assert compare([[1], [2, 3, 4]], [[1], 4]) >= 1
+    assert compare([9], [[8, 7, 6]]) <= -1
+    assert compare([[4, 4], 4, 4], [[4, 4], 4, 4, 4]) >= 1
+    assert compare([7, 7, 7, 7], [7, 7, 7]) <= -1
+    assert compare([], [3]) >= 1
+    assert compare([[[]]], [[]]) <= -1
 
     left = [1, [2, [3, [4, [5, 6, 7]]]], 8, 9]
     right = [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]
 
-    assert compare(left, right) is False
+    assert compare(left, right) <= -1
 
 
 def main():
     with open("day13.txt", "r", encoding="utf8") as file:
         data = file.read()
     print("Day 13a", day13a(data))
+    print("Day 13b", day13b(data))
 
 
 if __name__ == "__main__":
